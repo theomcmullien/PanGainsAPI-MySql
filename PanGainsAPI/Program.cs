d358841f7d5ef73fc113cb220de2ef1da8a93ffb
Riley Howley <71965268+Riley-Howley@users.noreply.github.com>
2022-05-31 17:12:32 +1200

    Added For loop

--- PanGainsAPI/Program.cs
+++ PanGainsAPI/Program.cs
@@ -65,6 +65,10 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    for (int i =0;i<100;i++) {
+        Console.WriteLine("Sup Pussy");
+    }
 }
 
 //Remove this line when Hosting will cause issues without SSL Cert
