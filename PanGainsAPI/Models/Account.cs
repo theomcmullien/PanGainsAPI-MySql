25d09a8de8da7281f3b108d53489c176c7fc7086
Theo McMullien <theomcmullien.16@gmail.com>
2022-06-06 04:06:02 +1200

    Reverted back (Added required)

--- PanGainsAPI/Models/Account.cs
+++ PanGainsAPI/Models/Account.cs
@@ -6,18 +6,28 @@ namespace PanGainsAPI.Models
     {
         [Key]
         public int AccountID { get; set; }
+        [Required]
         public string? Firstname { get; set; }
+        [Required]
         public string? Lastname { get; set; }
+        [Required]
         public string? Email { get; set; }
+        [Required]
         public string? Password { get; set; }
+        //[Required]
         //public byte[]? PasswordHash { get; set; }
+        //[Required]
         //public byte[]? PasswordSalt { get; set; }
         public string? Title { get; set; }
         public string? ProfilePicture { get; set; }
         public string? Description { get; set; }
+        [Required]
         public bool Private { get; set; }
+        [Required]
         public bool Notifications { get; set; }
+        [Required]
         public int AverageChallengePos { get; set; }
+        [Required]
         public string? Type { get; set; }
         public string? Role { get; set; } //JWT Signing Claims
     }
